708f650f75211d25cd263c34eea84e2f8e26b21b
Stephane Bounmy <stephanebounmy@gmail.com>
2014-02-01 00:31:48 +0100

    fix chimpy for frontend

--- app/controllers/spree/chimpy/subscribers_controller.rb
+++ app/controllers/spree/chimpy/subscribers_controller.rb
@@ -1,16 +1,18 @@
-class Spree::Chimpy::SubscribersController < ApplicationController
-  respond_to :html
+module Spree
+  class Chimpy::SubscribersController < StoreController
+    respond_to :html
 
-  def create
-    @subscriber = Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first_or_initialize
-    @subscriber.update_attributes(params[:chimpy_subscriber])
-    if @subscriber.save
-      Spree::Chimpy::Subscription.new(@subscriber).subscribe
-      flash[:notice] = I18n.t("spree.chimpy.subscriber.success")
-    else
-      flash[:error] = I18n.t("spree.chimpy.subscriber.failure")
+    def create
+      @subscriber = Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first_or_initialize
+      @subscriber.update_attributes(params[:chimpy_subscriber])
+      if @subscriber.save
+        Chimpy::Subscription.new(@subscriber).subscribe
+        flash[:notice] = I18n.t("spree.chimpy.subscriber.success")
+      else
+        flash[:error] = I18n.t("spree.chimpy.subscriber.failure")
+      end
+
+      respond_with @subscriber, location: request.referer
     end
-
-    respond_with @subscriber, location: request.referer
   end
 end
